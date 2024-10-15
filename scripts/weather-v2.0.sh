88bd46205da1fc2563aac32f2fa81f99887c2211
Chrono Lux <amber_c001@protonmail.com>
2024-10-14 23:32:03 -0600

    description lang

--- scripts/weather-v2.0.sh
+++ scripts/weather-v2.0.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# v2.0 Closebox73
 # This script is to get weather data from openweathermap.com in the form of a json file
 # so that conky will still display the weather when offline even though it doesn't up to date
 
@@ -14,7 +13,7 @@ api_key=e46d6b1c945f2e9983f0735f8928ea2f
 # choose between metric for Celcius or imperial for fahrenheit
 unit=metric
 
-# i'm not sure it will support all languange, 
+# support all languange : es, en, de, fr, pt, pt_br, ru (more information https://openweathermap.org/api/one-call-3#multi), 
 lang=en
 
 # Main command
